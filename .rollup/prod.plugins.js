5ed8d3f3bbd7a33589c243538a8ddd9444db67b6
Toan <me@toan.io>
2020-08-16 15:11:28 +0700

    add analyzer

--- .rollup/prod.plugins.js
+++ .rollup/prod.plugins.js
@@ -1,4 +1,5 @@
 import { terser } from 'rollup-plugin-terser'
 import cleaner from 'rollup-plugin-cleaner'
+import analyze from 'rollup-plugin-analyzer'
 
-export default () => [cleaner({ targets: ['./dist'] }), terser()]
+export default () => [cleaner({ targets: ['./dist'] }), terser(), analyze()]
