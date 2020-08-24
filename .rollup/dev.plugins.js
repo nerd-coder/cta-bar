e63afab29ee78f30fd3ccf9f5ac9ab4e43641cb9
Toan <me@toan.io>
2020-08-24 15:18:39 +0700

    update template

--- .rollup/dev.plugins.js
+++ .rollup/dev.plugins.js
@@ -3,7 +3,7 @@ import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 
 export default () => [
-  html({ template: 'src/template.html', target: 'index.html' }),
+  html({ template: 'src/template.html', target: 'index.html', attrs: ['async', 'defer'] }),
   serve('dist'),
   livereload('dist'),
 ]
