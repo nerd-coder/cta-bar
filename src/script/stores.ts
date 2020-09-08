7e44d8ae72f4ae42b650ce34b96584ad1fb32c6a
hung <Hung@Macbook-Pro.local>
2020-09-08 10:16:06 +0700

    btn right ok

--- src/script/stores.ts
+++ src/script/stores.ts
@@ -1,22 +1,29 @@
 export default function useMobileScroll(area,elmPrev,elmAfter,btnBack,btnNext){
+  const elmAfterX = elmAfter.getBoundingClientRect().x
+  if(elmAfterX >= 300){
+    btnNext.classList.remove("hide")
+  }else{
+    btnNext.classList.add("hide")
+  }
+
   const options = {
     root: area,
     rootMargin: '10px',
     threshold: 1.0
   }
+
   const observer = new IntersectionObserver((entries, observer) => {
     let isDisplay = true
     for (const entry of entries) {
       entry.boundingClientRect.x <= 0? isDisplay = false : isDisplay = true
       if(isDisplay){
         btnBack.classList.add("hide")
-        btnNext.classList.remove("hide")
         return
       }
       btnBack.classList.remove("hide")
-      btnNext.classList.add("hide")
     }
   }, options);
+  
   observer.observe(elmPrev)
 }
 
